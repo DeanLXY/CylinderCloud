303293e77725e3912a80ccf47958c317b01f3bbc
wangxujie <wangxj0714@163.com>
2015-10-21 11:24:31 +0800

    参数修正

--- app/src/main/java/com/example/cylindercloud/websocket/protocol/BottleCarBRequest.java
+++ app/src/main/java/com/example/cylindercloud/websocket/protocol/BottleCarBRequest.java
@@ -16,6 +16,7 @@ public class BottleCarBRequest extends IRequest {
 
     @Override
     protected String prapareMsg() {
-        return "token="+this.getToken()+",rfiduid=3da3b81f,infotab=_color,method=0";
+        return "rfiduid=3da3b81f&token=" + this.getToken() + "_cccc_cphm_djbh_cpxh_cjh_fdjh_syxz_color_regUnit_gd_zzgb_jybh_zzdm_zzxkzbh_ccrq_zcbh_xjrq_bfrq_azdw_cqjz_CNGID_unitName_licenseNO_licEnd_updateTime";
+//        return "token="+this.getToken()+",rfiduid=3da3b81f,infotab=_color,method=0";
     }
 }
